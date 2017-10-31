038d9ae0af909f40d26001681a579f50ab1d71f4
jychenX <ion9210cjy@163.com>
2017-10-31 12:02:01 +0800

    iOS v4.0.3  Android v3.1.1

--- proj.android-studio/app/src/cn/sharesdk/onekeyshare/themes/classic/FriendListItem.java
+++ proj.android-studio/app/src/cn/sharesdk/onekeyshare/themes/classic/FriendListItem.java
@@ -45,14 +45,14 @@ public class FriendListItem extends LinearLayout {
 		setBackgroundColor(0xffffffff);
 
 		ivCheck = new ImageView(context);
-		LayoutParams lp = new LayoutParams(
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
 				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		lp.gravity = Gravity.CENTER_VERTICAL;
 		addView(ivCheck, lp);
 
 		aivIcon = new AsyncImageView(context);
 		int avatarWidth = (int) (ratio * DESIGN_AVATAR_WIDTH);
-		lp = new LayoutParams(avatarWidth, avatarWidth);
+		lp = new LinearLayout.LayoutParams(avatarWidth, avatarWidth);
 		lp.gravity = Gravity.CENTER_VERTICAL;
 		int avatarMargin = (int) (ratio * DESIGN_AVATAR_PADDING);
 		lp.setMargins(avatarMargin, 0, avatarMargin, 0);
@@ -62,7 +62,7 @@ public class FriendListItem extends LinearLayout {
 		tvName.setTextColor(0xff000000);
 		tvName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
 		tvName.setSingleLine();
-		lp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		lp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		lp.gravity = Gravity.CENTER_VERTICAL;
 		lp.weight = 1;
 		addView(tvName, lp);
